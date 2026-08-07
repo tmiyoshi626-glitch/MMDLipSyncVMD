8433ccb367a53a409a576bead73867f1fdfb3fa0
tmiyoshi626-glitch <tmiyoshi626@gmail.com>
2026-08-07 15:50:03 +0000

    Add PMX texture and material tests

--- tests/test_pmx.py
+++ tests/test_pmx.py
@@ -17,6 +17,10 @@ def test_read_pmx_model():
     assert model.model_name_en is not None
     assert model.vertex_count > 0
     assert model.face_index_count > 0
+    assert len(model.textures) == 26
+    assert model.textures[0] == "Texture\\body_Teto.png"
+    assert len(model.materials) == 45
+    assert model.materials[0] == "Body"
 
 # TODO:
 # Add tests for faces, textures, materials, bones and morphs.
